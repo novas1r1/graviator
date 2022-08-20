4eb1d7339f7e19fa2e39413ce8bd06d346afa0f2
Alejandro Santiago <dev@alestiago.com>
2022-08-20 21:36:52 +0100

    feat: implemented jetpack (#32)
    * feat: implemented jetpack
    
    * Merge remote-tracking branch 'origin' into feat/jetpack

--- lib/game/helpers/body_entity.dart
+++ lib/game/helpers/body_entity.dart
@@ -1,14 +1,15 @@
 // ignore_for_file: public_member_api_docs
 
+import 'package:flame/components.dart';
 import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 
-class BodyEntity extends Entity {
+class BodyEntity<T extends BodyComponent> extends Entity {
   BodyEntity({
-    super.children,
+    Iterable<Component>? children,
     super.behaviors,
     required this.bodyComponent,
-  }) : assert(
+  })  : assert(
           () {
             if (children != null) {
               return children.whereType<BodyComponent>().isEmpty;
@@ -16,13 +17,19 @@ class BodyEntity extends Entity {
             return true;
           }(),
           'Children must not contain a BodyComponent.',
-        );
+        ),
+        _children = children;
 
-  final BodyComponent bodyComponent;
+  final T bodyComponent;
+
+  Type get type => T;
+
+  final Iterable<Component>? _children;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     await add(bodyComponent);
+    if (_children != null) await bodyComponent.addAll(_children!);
   }
 }
