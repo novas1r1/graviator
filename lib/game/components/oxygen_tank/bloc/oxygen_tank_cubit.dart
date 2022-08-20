127c2bfbf3104b2dbdafa564546f92e417087a1a
Verena Zaiser <development@verena-zaiser.de>
2022-08-20 22:19:05 +0200

    renamed oxygen tank cubit

--- lib/game/components/oxygen_tank/bloc/oxygen_tank_cubit.dart
+++ lib/game/components/oxygen_tank/bloc/oxygen_tank_cubit.dart
@@ -1,10 +1,10 @@
 import 'package:bloc/bloc.dart';
 
-/// The [OxygenTank] cubit contains all information about the level of oxygen the player still has.
+/// The [OxygenTankCubit] cubit contains all information about the level of oxygen the player still has.
 /// It is possible to inflate and deflate the Oxygen in the game with tanks
-class OxygenTank extends Cubit<int> {
+class OxygenTankCubit extends Cubit<int> {
   /// Creates the cubit
-  OxygenTank(super.initialState);
+  OxygenTankCubit(super.initialState);
 
   /// Increase the current amount of the oxygen tank
   void inflated(int value) {
