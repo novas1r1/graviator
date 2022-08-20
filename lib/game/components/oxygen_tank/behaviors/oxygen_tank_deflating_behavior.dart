3f51d26c0ce74f328e20901f19864488920cd4bb
Verena Zaiser <development@verena-zaiser.de>
2022-08-20 22:17:13 +0200

    clenaup

--- lib/game/components/oxygen_tank/behaviors/oxygen_tank_deflating_behavior.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flame_behaviors/flame_behaviors.dart';
-
-import '../oxygen_tank.dart';
-
-/// Reduce the amount of oxygen in the tank
-class OxygenTankDeflatingBehavior extends Behavior<OxygenTank>{
-
-}
