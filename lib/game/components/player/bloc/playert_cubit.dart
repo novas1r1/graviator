d850c02fc8333298d82fe805e84d1f5cf825ff2c
Verena Zaiser <development@verena-zaiser.de>
2022-08-21 09:32:17 +0200

    added basic cubit

--- lib/game/components/player/bloc/playert_cubit.dart
+++ lib/game/components/player/bloc/playert_cubit.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -12,5 +14,32 @@ class PlayerCubit extends Cubit<PlayertState> {
 
   void reduceOxygen(int value) {
     emit(state.copyWith(oxygen: state.oxygen - value));
+  }
+
+  void die() {
+    emit(
+      state.copyWith(playerStatus: PlayerStatus.dead, health: 0, oxygen: 0),
+    );
+  }
+
+  void revive() {
+    emit(
+      state.copyWith(
+        playerStatus: PlayerStatus.alive,
+        health: 100,
+        oxygen: 100,
+      ),
+    );
+  }
+
+  /// Player dies if health drops below 0 after  taking [damage]
+  void takeDamage(int damage) {
+    final currentHealth = state.health - damage;
+
+    if (currentHealth <= 0) {
+      die();
+    } else {
+      emit(state.copyWith(health: currentHealth));
+    }
   }
 }
--- lib/game/components/player/bloc/playert_state.dart
+++ lib/game/components/player/bloc/playert_state.dart
@@ -1,18 +1,33 @@
+// ignore_for_file: public_member_api_docs
+
 part of 'playert_cubit.dart';
+
+enum PlayerStatus {
+  alive,
+  dead,
+}
 
 @immutable
 class PlayertState {
   const PlayertState({
     this.oxygen = 100,
+    this.health = 100,
+    this.playerStatus = PlayerStatus.alive,
   });
 
   final int oxygen;
+  final int health;
+  final PlayerStatus playerStatus;
 
   PlayertState copyWith({
     int? oxygen,
+    int? health,
+    PlayerStatus? playerStatus,
   }) {
     return PlayertState(
       oxygen: oxygen ?? this.oxygen,
+      health: health ?? this.health,
+      playerStatus: playerStatus ?? this.playerStatus,
     );
   }
 }
