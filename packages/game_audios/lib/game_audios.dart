6540491f20c0d9a2505085c1f09916747a9cc71c
Max Weber <flutterexp@gmail.com>
2022-08-21 16:21:44 +0200

    feat: Created a package for the audio files

---
+++ packages/game_audios/lib/game_audios.dart
@@ -0,0 +1,3 @@
+library game_audios;
+
+export 'src/game_audios.dart';
---
+++ packages/game_audios/lib/src/game_audios.dart
@@ -0,0 +1,7 @@
+/// {@template game_audios}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class GameAudios {
+  /// {@macro game_audios}
+  const GameAudios();
+}
