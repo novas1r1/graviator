a49df9c40a07b79c0732f6eb86173ed608d60caf
Max Weber <flutterexp@gmail.com>
2022-08-21 17:00:30 +0200

    feat: Added sounds packages folder and platform / barrels

---
+++ lib/game/components/barrel/behaviors/behaviors.dart
@@ -0,0 +1,1 @@
+export 'barrel_bouncing_behavior.dart';
--- packages/game_audios/lib/src/game_audios.dart
+++ packages/game_audios/lib/src/game_audios.dart
@@ -1,7 +1,33 @@
-/// {@template game_audios}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class GameAudios {
-  /// {@macro game_audios}
-  const GameAudios();
-}
+// /*
+// /// Sounds available to play.
+// enum GameAudio { pain_one, pain_two, plop_one, plop_two, plop_three, plop_four }
+//
+// /// Defines the contract of the creation of an [AudioPool].
+// typedef CreateAudioPool = Future<AudioPool> Function(
+//   String sound, {
+//   bool? repeating,
+//   int? maxPlayers,
+//   int? minPlayers,
+//   String? prefix,
+// });
+//
+// /// Defines the contract for playing a single audio.
+// typedef PlaySingleAudio = Future<void> Function(String, {double volume});
+//
+// /// Defines the contract for looping a single audio.
+// typedef LoopSingleAudio = Future<void> Function(String, {double volume});
+//
+// /// Defines the contract for pre fetching an audio.
+// typedef PreCacheSingleAudio = Future<void> Function(String);
+//
+// /// Defines the contract for configuring an [AudioCache] instance.
+// typedef ConfigureAudioCache = void Function(AudioCache);
+//
+// /// {@template game_audios}
+// /// A Very Good Project created by Very Good CLI.
+// /// {@endtemplate}
+// class GameAudios {
+//   /// {@macro game_audios}
+//   const GameAudios();
+// }
+// */
